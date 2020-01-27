89b4be2ec0912639b08c3e94e4ae928b2524681b
Peter Fossey <peter@fossweb.net>
2020-01-27 19:51:06 +0000

    functions.php

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+
+function redirect($url)
+{
+    $string = '<script type="text/javascript">';
+    $string .= 'window.location = "' . $url . '"';
+    $string .= '</script>';
+
+    echo $string;
+}
+
+function seoUrl($string) {
+    //Lower case everything
+    $string = strtolower($string);
+    //Make alphanumeric (removes all other characters)
+    $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+    //Clean up multiple dashes or whitespaces
+    $string = preg_replace("/[\s-]+/", " ", $string);
+    //Convert whitespaces and underscore to dash
+    $string = preg_replace("/[\s_]/", "-", $string);
+    return $string;
+}
+
+?>
--- index.php
+++ index.php
@@ -34,17 +34,7 @@
 
 <?php
 
-function seoUrl($string) {
-    //Lower case everything
-    $string = strtolower($string);
-    //Make alphanumeric (removes all other characters)
-    $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
-    //Clean up multiple dashes or whitespaces
-    $string = preg_replace("/[\s-]+/", " ", $string);
-    //Convert whitespaces and underscore to dash
-    $string = preg_replace("/[\s_]/", "-", $string);
-    return $string;
-}
+include 'functions.php';
 
 $urlroot = 'http://go.njpc.it/prst-';
 $desc = $_GET['desc'];
@@ -129,3 +119,7 @@ curl_close($handle);
 </footer>
 </body>
 </html>
+
+<?php
+	sleep(10);
+?>
