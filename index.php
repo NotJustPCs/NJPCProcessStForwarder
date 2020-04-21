b62d3ff537f7760c0e0a8aed64c70803e1e5df36
Peter Fossey <peter@fossweb.net>
2020-04-21 17:18:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -91,7 +91,6 @@ if($httpCode == 404) {
 	echo '<i class="fa fa-building" aria-hidden="true"></i> <strong>Organisation of Ticket: </strong>' . $org . '<br>';
 	echo '<i class="fa fa-ticket" aria-hidden="true"></i> <strong>Original Ticket Description: </strong>' . $originaldesc . '<br>';
 	echo '<i class="fa fa-file-code-o" aria-hidden="true"></i> <strong>Process URL tested: </strong>' . $desc . '<br>';
-	echo $response;
 	echo '<i class="fa fa-list-ol" aria-hidden="true"></i> <strong>Word numbers of Ticket Description: </strong><br><table>';
 	$descbitsid = 0;
 	foreach($descbits as $row){
@@ -116,6 +115,7 @@ if($httpCode == 404) {
 	echo '<i class="fa fa-building" aria-hidden="true"></i> <strong>Organisation of Ticket: </strong>' . $org . '<br>';
 	echo '<i class="fa fa-ticket" aria-hidden="true"></i> <strong>Original Ticket Description: </strong>' . $originaldesc . '<br>';
 	echo '<i class="fa fa-file-code-o" aria-hidden="true"></i> <strong>Process URL: </strong>' . $desc . '<br>';
+	echo $response;
 	echo '<i class="fa fa-play" aria-hidden="true"></i> <a href=' . $desc . '>Click here</a> to start the process if you are not redirected.';
 }
 
