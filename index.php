1df601d6c98a1611f57abde617ce30a7fb0faa8b
foshdafosh <peter@notjustpcs.co.uk>
2020-06-01 13:32:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ $descbits = explode(" ",$desc);
 $originaldesc = $_GET['desc'];
 $org = $_GET['org'];
 $ticketno = $_GET['ticketno'];
-$ticketnicename = '#' . $ticketno . ' ' . $originaldesc
+$ticketnicename = '#' . $ticketno . ' ' . $originaldesc;
 
 if (($org == 'eBay') && ($descbits[1] == 'sold')) {
     $desc = $org . $descbits[1];
@@ -117,7 +117,7 @@ if($httpCode == 404) {
 	curl_setopt($targeturlhandle, CURLOPT_FOLLOWLOCATION, true);
 	curl_exec($targeturlhandle);
 	$targeturl = curl_getinfo($targeturlhandle, CURLINFO_EFFECTIVE_URL);
-	$targeturlincquery = $targeturl;
+	$targeturlincquery = $targeturl . '?checklist_name=' . urlencode($ticketnicename);
 	echo '<i style="color:green;" class="fa fa-check" aria-hidden="true"></i> Starting your new process in a mo.<br><b>- Please activate the Share link and copy it into the ticket.<br>- Please rename the process to include the ticket name</b><br>';
 	echo '<i class="fa fa-building" aria-hidden="true"></i> <strong>Organisation of Ticket: </strong>' . $org . '<br>';
 	echo '<i class="fa fa-ticket" aria-hidden="true"></i> <strong>Original Ticket Number: </strong>' . $ticketno . '<br>';
