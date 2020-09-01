373629a5018ef96b27f87c0688bfa7708b23d2ef
foshdafosh <peter@notjustpcs.co.uk>
2020-09-01 11:06:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -93,6 +93,9 @@ if (($org == 'eBay') && ($descbits[1] == 'sold')) {
 			elseif (($descbits[2] == 'AutoSSL') && ($descbits[3] == 'reduced')) {
 				$desc = 'autossl-reduced-ssl-coverage';
 			}
+			elseif (($descbits[1] == 'Disk') && ($descbits[2] == 'Usage') && ($descbits[2] == 'user')) {
+				$desc = 'autossl-reduced-ssl-coverage';
+			}
 		}
 	}
 
